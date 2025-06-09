858b2a7e57533a7d6aa815d9ecc5b8e3e6cb75b0
GabrielMaxxdog <gabrielmaxx@bol.com.br>
2025-06-09 16:22:58 -0300

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "../components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
+import { PhoneCall, ShoppingCart, Rocket, MessageSquareText } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>iStore Brasil - Página em construção</h1>
+    </div>
+  );
+}
